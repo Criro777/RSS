059f711b949dd339873933438f619de42f420d34
Guiliano <test@pro.ru>
2017-02-28 21:26:06 +0400

    Добавлены новые маршруты с учётом пагинации страниц

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ use vendor\core\Router;
 
 
 require '../vendor/autoload.php';
+require '../vendor/components/Pagination.php';
+require '../vendor/components/Translit.php';
 
 define('ROOT', dirname(__DIR__));
 define('APP', dirname(__DIR__) . '/app');
@@ -18,8 +20,12 @@ $query = $_SERVER['QUERY_STRING'];
 /*Маршруты по умолчанию*/
 
 Router::add('^$', ['controller' => 'Article', 'action' => 'index']);
-Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?/?(?P<parameter>[0-9]+)?$');
+
+Router::add('^(?P<controller>[a-z-]+)/(?P<action>[a-z-]+)/page-/?(?P<parameter>[0-9]+)?$');
+Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?/?(?P<parameter>[a-zA-Z0-9\-]+)?$');
 Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
+
+
 
 /*определяем текущий маршрут*/
 
