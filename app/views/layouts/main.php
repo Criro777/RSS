a23a5089a9ec32431125f8d84ec10b4dc90b0618
Guiliano <test@pro.ru>
2017-03-07 12:38:22 +0400

    Добавлена ссылка на скрипт валидации в шаблоне

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -2,17 +2,11 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>MAIN</title>
         <link rel="stylesheet" type="text/css" href="/public/css/main.css"/>
-        <link href="/public/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="/public/bootstrap/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-
-        <![endif]-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        
     </head>
     <body>
 
@@ -32,8 +26,8 @@
     </div>
     
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="/public/bootstrap/js/bootstrap.min.js"></script>
-    <script src="/public/js/main.js"></script>
+        <script type="text/javascript" src="/public/js/jquery-1.11.2.min.js"></script>
+        <script type="text/javascript" src="/public/js/validator.min.js"></script>
+        <script type="text/javascript" src="/public/js/main.js"></script>
     </body>
 </html>
